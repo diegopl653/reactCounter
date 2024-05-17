acd260631b6defca7add4a11f8be2469a158d32b
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2024-05-17 14:52:01 -0400

    Feat: add phase 1 addition and subtraction

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [valor, setValor] = useState(0);
+  
 
   return (
     <>
-      
+    <div className="container">
+      <button className="btn" onClick={() => setValor(valor+1)}>Sumar</button>
+      <p>{valor}</p>
+      <button className="btn" onClick={() => setValor(valor-1)}>Restar</button>
+    </div>
+      <button className="btn" onClick={() => setValor(0)}>Reset</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
